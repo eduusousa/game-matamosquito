f749893a98fc15f94196396aff9d2953dbc9de10
Eduardo Sousa <dusousa1412@gmail.com>
2022-09-13 20:06:55 -0300

    Finalizando o jogo, adicionando novas funcionalidades: tempo, pontuação e telas adicionais

--- game.js
+++ game.js
@@ -1,12 +1,47 @@
 var width = 0      //Largura
 var height = 0     //Altura
 
+var lifes = 1
+var points = 0
+var timer = 30
+
+var criacaoMosquito = 2000
+
+document.getElementById('time').innerHTML = timer
+document.getElementById('score').innerHTML = points
+
+
+let dificuldade = window.location.search
+dificuldade = dificuldade.replace('?', '')
+
+if(dificuldade === ""){
+    alert("Selecione um nível...")
+    window.location.href = 'index.html'
+}
+
+if(dificuldade === 'facil') {
+    criacaoMosquito = 2000
+
+} else if(dificuldade === 'medio'){
+    criacaoMosquito = 1000
+
+} else if(dificuldade === 'dificil'){
+    criacaoMosquito = 900
+}
+
+
+function startGame() {
+    let nivel = document.getElementById('nivel').value
+    window.location.href = 'main.html?' + nivel
+}
+
 
 //Ajuste do tamanho da tela para o jogo
 function sreenAdjustmentGame() {
     width = window.innerWidth
     height = innerHeight
 }
+
 
 //Define qual classe colocar no elemento do Mosquito
 function sizeRandom() {
@@ -26,6 +61,7 @@ function sizeRandom() {
 
 }
 
+
 //Define qual a direção do Mosquito
 function sideRandom() {
 
@@ -41,12 +77,34 @@ function sideRandom() {
 
 }
 
+
+var cronometro = setInterval(function(){
+    timer--
+    if(timer < 0){
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href = 'winner.html'
+    }   else {
+        document.getElementById('time').innerHTML = timer
+    }
+}, 1000)
+
+
 //Define uma posição aleatória para a imagem do mosquito
 function positionRandom() {
 
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if(lifes > 3){
+            window.location.href = 'game-over.html'
+        } else {
+            document.getElementById('life' + lifes).src = "imagens/coracao_vazio.png"
+            lifes++
+        }
+
     }
+
 
     var positionAxisX = Math.floor(Math.random() * width) - 90
     var positionAxisY = Math.floor(Math.random() * height) - 90
@@ -63,15 +121,19 @@ function positionRandom() {
     mosquito.style.transform = 'scaleX(' + sideRandom() + ')'
     mosquito.style.position  = 'absolute'
 
+    mosquito.onclick = function(){
+        points++
+        document.getElementById('score').innerHTML = points
+        this.remove()
+    }
+
     document.body.appendChild(mosquito)
 
 }
 
 
-
 sreenAdjustmentGame()
-
-setInterval(()=>{
+var criaMosquito = setInterval(()=>{
     positionRandom()
-}, 1000)
+}, criacaoMosquito)
 
